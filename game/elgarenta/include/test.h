2065c38d2343f0ffca49dc2d5ab6cfab66848d67
SergeyMC9730 <bdsservers@yandex.ru>
2022-09-10 15:00:06 +0800

    Hello, World program

---
+++ game/elgarenta/include/test.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifdef __CPLUSPLUS
+extern "C" {
+#endif
+
+void simpleTest(void);
+
+#ifdef __CPLUSPLUS
+}
+#endif
---
+++ game/elgarenta/src/test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+#include <test.h>
+
+void simpleTest(void) {
+    printf("Hello, World!\n");
+}
